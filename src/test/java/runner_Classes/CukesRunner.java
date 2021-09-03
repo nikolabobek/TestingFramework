0e27969547c7e82238b9f590e7eedd7bbc183bb4
Nikola <nikolabobwk@gmail.com>
2021-09-02 22:33:29 -0400

    sample commit

---
+++ src/test/java/runner_Classes/CukesRunner.java
@@ -0,0 +1,21 @@
+package runner_Classes;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"html:target/cucumber-report.html",
+                 "json:target/cucumber.json"
+        },
+        glue = "step_definitions",
+        features = "src/test/resources/features/"
+
+)
+
+public class CukesRunner {
+
+
+
+}
---
+++ src/test/resources/features/sample.feature
@@ -0,0 +1,5 @@
+Feature: Sample
+
+  @wip
+  Scenario: Sample
+    Given sample step
